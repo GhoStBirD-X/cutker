f00257d1cd39c26599e9ed3b4077803bdd53e900
GhoStBirD-X <fallahimam356@gmail.com>
2026-09-16 15:14:33 +0700

    Logo icon

--- resources/js/components/app-logo-full.tsx
+++ resources/js/components/app-logo-full.tsx
@@ -1,5 +1,20 @@
+import logoFullDark from '@/images/logo-kkp-full-dark.png';
 import logoFull from '@/images/logo-kkp-full.png';
+import { cn } from '@/lib/utils';
 
 export default function AppLogoFull({ className }: { className?: string }) {
-    return <img src={logoFull} alt="KKP Inovasi" className={className} />;
+    return (
+        <>
+            <img
+                src={logoFull}
+                alt="KKP Inovasi"
+                className={cn(className, 'dark:hidden')}
+            />
+            <img
+                src={logoFullDark}
+                alt="KKP Inovasi"
+                className={cn(className, 'hidden dark:block')}
+            />
+        </>
+    );
 }
